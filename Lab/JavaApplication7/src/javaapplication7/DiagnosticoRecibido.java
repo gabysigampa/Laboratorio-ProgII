a5b7f79d0d2ad560e93528c7b8d524427aec0e1d
Braian Salavarria <Sigampag@gamail.com>
2018-11-21 07:46:50 -0300

    diagnostico  Recivido

---
+++ Lab/JavaApplication7/src/javaapplication7/DiagnosticoRecibido.java
@@ -0,0 +1,14 @@
+
+package javaapplication7;
+
+/**
+ *
+ * @author Braian
+ */
+public class DiagnosticoRecibido {
+    //resultadoatencion
+    //atencion Brindada
+    //diagnstico
+    //gravedad paciente
+    
+}
